b6c1c6d4b9d449f18f0a454c470b95df87450b40
Kel Byers <kel_byers@homedepot.com>
2021-06-01 10:44:02 -0500

    pyenv path update

--- .profile.d/env_py.zsh
+++ .profile.d/env_py.zsh
@@ -1,11 +1,12 @@
 # spaceship prompt handles pyenv virtualenv prompts
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 export PYENV_ROOT=/usr/local/var/pyenv
+export PATH="$PYENV_ROOT/bin:$PATH"
 
-if which pyenv >/dev/null; then
+if which pyenv > /dev/null; then
     eval "$(pyenv init - zsh)"
     alias pyenv='nocorrect pyenv'
 fi
-if which pyenv-virtualenv-init >/dev/null; then
+if which pyenv-virtualenv-init > /dev/null; then
     eval "$(pyenv virtualenv-init - zsh)"
 fi
