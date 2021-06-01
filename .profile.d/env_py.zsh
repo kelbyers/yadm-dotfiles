1d95e447471dc4065760a14c515171e46f24ebee
Kel Byers <kel_byers@homedepot.com>
2021-06-01 10:57:15 -0500

    make sure pyenv virtualenv works

--- .profile.d/env_py.zsh
+++ .profile.d/env_py.zsh
@@ -4,7 +4,8 @@ export PYENV_ROOT=/usr/local/var/pyenv
 export PATH="$PYENV_ROOT/bin:$PATH"
 
 if which pyenv > /dev/null; then
-    eval "$(pyenv init - zsh)"
+    eval "$(pyenv init --path)" # this only sets up the path stuff
+    eval "$(pyenv init -)"      # this makes pyenv work in the shell
     alias pyenv='nocorrect pyenv'
 fi
 if which pyenv-virtualenv-init > /dev/null; then
