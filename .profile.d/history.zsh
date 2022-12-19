5caa6325ded03187292c5784070f4cc4a4e81085
Kel Byers <kel_byers@homedepot.com>
2022-12-19 09:49:21 -0600

    only check history size when making backup

--- .profile.d/history.zsh
+++ .profile.d/history.zsh
@@ -4,18 +4,18 @@ export HISTFILE_BACKUP
 function _CMD { /bin/cp -fa ${HISTFILE} ${HISTFILE_BACKUP} }
 
 if [[ -f ${HISTFILE_BACKUP} ]]; then
-    _A=$(zstat +size ${HISTFILE})
-    _B=$(zstat +size ${HISTFILE_BACKUP})
+        _TS=$(zstat +mtime ${HISTFILE_BACKUP})
+        if ((${_TS} < $(/bin/date -v -1d +%s))); then
+            _A=$(zstat +size ${HISTFILE})
+            _B=$(zstat +size ${HISTFILE_BACKUP})
 
-    if (((_A < _B) && ((_B * 100 / _A) > 120))); then
-        cat >&2 << EOF
+            if (( (_A < _B) && ((_B * 100 / _A) > 120) )); then
+                cat >&2 << EOF
 !!! WARNING !!!
 zsh history file seems corrupted!
 EOF
-        /bin/ls -l ${HISTFILE} ${HISTFILE_BACKUP}
-    else
-        _TS=$(zstat +mtime ${HISTFILE_BACKUP})
-        if ((${_TS} < $(/bin/date -v -1d +%s))); then
+            /bin/ls -l ${HISTFILE} ${HISTFILE_BACKUP}
+        else
             _CMD
         fi
         unset _TS
