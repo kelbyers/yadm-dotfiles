26eca3d6ec1ef2c404b3ead3fbb09e83b48fc18f
kel <ruferto@gmail.com>
2023-01-08 16:16:34 -0600

    gnu uname output different on cygwin

--- .spaceshiprc.zsh
+++ .spaceshiprc.zsh
@@ -1,6 +1,6 @@
 ## Spaceship Prompt Customizations
 SPACESHIP_EXIT_CODE_SHOW=true
 SPACESHIP_HOST_SHOW=always
-if [[ "$(uname -s)" = "Cygwin" ]]; then
+if [[ "$(uname -s)" = "CYGWIN"* ]]; then
     SPACESHIP_PROMPT_ASYNC=false
 fi
