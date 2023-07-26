377836dc6aaf0d20d73c6361620432d180688353
Kel Byers <kel_byers@homedepot.com>
2023-07-26 15:06:40 -0500

    two versions of java sdk on M1

--- .profile.d/env_j.sh
+++ .profile.d/env_j.sh
@@ -1,3 +1,11 @@
-if which jenv >/dev/null; then
-    eval "$(jenv init -)"
-fi
+export JAVA_HOME=$(/usr/libexec/java_home -v17)
+
+export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+export JAVA_17_HOME=$(/usr/libexec/java_home -v17)
+
+alias java8='export JAVA_HOME=$JAVA_8_HOME'
+alias java17='export JAVA_HOME=$JAVA_17_HOME'
+
+# if which jenv >/dev/null; then
+#     eval "$(jenv init -)"
+# fi
