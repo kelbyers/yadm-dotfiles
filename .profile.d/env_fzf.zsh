98da84663417c061da79da85e3653b38463dd616
Kel Byers <kel_byers@homedepot.com>
2024-11-01 15:40:10 -0500

    fzf-zsh-plugin doesn't use fzf from homebrew

--- .profile.d/env_fzf.zsh
+++ .profile.d/env_fzf.zsh
@@ -1,3 +1,12 @@
+if [[ $(command -v fzf) == */homebrew/* ]]; then
+    export FZF_PATH=$(readlink -f $(command -v fzf))
+    # ZSH expansions:
+    # ${FZF_PATH:P}  # resolve symlinks to the actual path
+    # ${FZF_PATH:h}  # remove the final path component
+    # ${FZF_PATH:h:h}  # remove the final two path components
+    FZF_PATH=${FZF_PATH:P:h:h}
+fi
+
 # Set up fzf key bindings and fuzzy completion
 if which fzf > /dev/null; then
     eval "$(fzf --zsh)"
